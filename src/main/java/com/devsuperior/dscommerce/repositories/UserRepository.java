3bac1453ab315e7fc4cfabfe663361d6cea889f4
Renata Viotto Hassum <renatahassum@gmail.com>
2022-10-12 11:41:46 -0300

    UserDetails, GrantedAuthority, UserDetailsService|Armazenamento senha codificada com BCrypt|Checklist do OAuth2 com JWT no Spring Boot

--- src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -1,14 +1,9 @@
 package com.devsuperior.dscommerce.repositories;
 
-import com.devsuperior.dscommerce.entities.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
+import com.devsuperior.dscommerce.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Query("SELECT obj FROM Product obj " +
-            "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%')) ")
-    Page<Product> searchByName(String name, Pageable pageable);
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
